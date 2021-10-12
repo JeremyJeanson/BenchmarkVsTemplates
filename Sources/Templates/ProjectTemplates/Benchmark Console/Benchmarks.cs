f9c1df59d56a1605cc07bba6474d7be81ea64f13
Jeremy Jeanson <jeremy.jeanson@live.fr>
2021-10-13 01:04:50 +0200

    Add first c# template + VS Extension

---
+++ Sources/Templates/ProjectTemplates/Benchmark Console/Benchmarks.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+
+namespace $safeprojectname$
+{
+    public class Benchmarks
+    {
+        [Benchmark(Baseline = true)]
+        public void Scenario1()
+        {
+            // Implement your benchmark here
+        }
+
+        [Benchmark]
+        public void Scenario2()
+        {
+            // Implement your benchmark here
+        }
+    }
+}
