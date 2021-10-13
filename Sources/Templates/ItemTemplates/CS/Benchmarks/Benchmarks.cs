34e5365af2bd7505d04dfc33252842858f1c89e7
Jeremy Jeanson <jeremy.jeanson@live.fr>
2021-10-13 21:51:30 +0200

    Add the C# Item template for Benchmarks

---
+++ Sources/Templates/ItemTemplates/CS/Benchmarks/Benchmarks.cs
@@ -0,0 +1,21 @@
+﻿using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+using System;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$
+    {
+        [Benchmark(Baseline = true)]
+        public void Scenario1()
+        {
+            // Implement your benchmark here
+        }
+
+        [Benchmark]
+        public void Scenario2()
+        {
+            // Implement your benchmark here
+        }
+    }
+}
